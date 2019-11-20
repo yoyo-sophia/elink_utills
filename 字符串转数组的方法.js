c6f30f1a0c0acb6b4acea44ca071c499ef1c4654
yoyoyo <594372929@qq.com>
2019-11-20 14:09:40 +0800

    fixed bug

--- 字符串转数组的方法.js
+++ 字符串转数组的方法.js
@@ -1,7 +1,29 @@
 // split 方法
-let str = 'test';
-let strArr1 = str.split('');
-let strArr2 = [...str];
+// let str = 'test';
+// let strArr1 = str.split('');
+// let strArr2 = [...str];
+//
+// console.log(strArr1);
+// console.log(strArr2);
 
-console.log(strArr1);
-console.log(strArr2);
+function matchDiff(originVal, editedVal, type) {
+  let maxLength = originVal.length >= editedVal.length ? originVal.length : editedVal.length;
+  let diffCount = 0;
+  for (let i = 0; i < maxLength; i++) {
+    if (originVal[i] !== editedVal[i]) {
+      diffCount++;
+    }
+  }
+  if (type === 'name' && (diffCount / maxLength) > 0.5) return true;
+  if (type === 'id' && (diffCount / maxLength) > 0.2) return true;
+  return false
+};
+
+
+let originVal = '钟辰乐';
+let editedVal = '钟雷';
+
+
+let diff = matchDiff(originVal, editedVal, 'name');
+console.log(diff)
+// console.log();
